a8c6892ad3021a24eda6394d49ce6f51128faa46
nkhachan <noopur.khachane@gmail.com>
2018-11-13 20:45:56 -0800

    working scrolling demo

--- QuickDemo.py
+++ QuickDemo.py
@@ -6,30 +6,40 @@
 
 '''
 
-
+import time
 from TronAPI import *
 
 def quickDemo():
     address = input("Please enter your public address  \n")
 
-    transactions = []
+    cachedhashes = []
 
     balance = str(getbalance(address))
-    print("Your current balance is : ", str(formatBalance(balance)), "TRX", "\n")
 
-    print ("Your most recent transactions are the following \n ")
+    if (balance != str(False)):
+        print("Your current balance is : ", str(formatBalance(balance)), "TRX", "\n")
+        print("Your most recent transactions are the following \n ")
+        for transaction in gettransactionobjects(address):
+            cachedhashes.append(transaction.hash)
+            print(transaction)
 
-    for transaction in gettransactionobjects(address):
-        transactions.append(transaction)
-        print(transaction)
+    else :
+        print("You have no balance in your account")
 
     while(1):
-        if (len(gettransactionobjects(address)) == len(transactions)):
-            pass
-        else :
-            lastTransaction = getlasttransactionobject(address)
-            print(lastTransaction)
-            transactions.append(lastTransaction)
+        time.sleep(2)
+
+        newtransactions = gettransactionobjects(address)
+        if (newtransactions):
+
+            if (len(newtransactions) > len(cachedhashes)):
+                pass
+
+            else:
+                lastTransaction = getlasttransactionobject(address)
+                if (lastTransaction.hash not in cachedhashes):
+                    print(lastTransaction)
+                    cachedhashes.append(lastTransaction.hash)
 
 
 
--- TronAPI.py
+++ TronAPI.py
@@ -10,47 +10,75 @@ full_node         = 'wallet'
 api               = 'api'
 
 
+def validresponse(response):
+    '''
+    Check if the response is a valid response
+    :param response:
+    :return:
+    '''
+    if (len(response.json())):
+        return True
+    return False
+
 def getbalance(address):
+    '''
+
+        Get the balance of an address
+
+    :param address:
+    :return:
+    '''
     hexstring = base58tohexstring(address)
     response = requests.get(gridendpoint + '/' + solidity_node + '/' +
                             "getaccount", params = {"address" : hexstring})
-    return response.json()["balance"]
+    if (validresponse(response)):
+        return response.json()["balance"]
+    return False
 
 
 def gettransactions(address):
     '''
 
-        GetTransaction API has following elements in each transaction :
+        GetTransaction API
 
-                {
-                  "hash": "string",
-                  "block": 0,
-                  "timestamp": "2018-11-13T20:48:28.782Z",
-                  "confirmed": true,
-                  "ownerAddress": "string",
-                  "toAddress": "string",
-                  "contractData": {
-                    "boolean": true,
-                    "number": true,
-                    "string": true,
-                    "object": true,
-                    "array": true,
-                    "null": true
-                  },
-                  "contractType": 0,
-                  "data": "string"
-                }
+    :param address:
+    :return:
 
+            A list of transfers (dictionaries) with the following info:
+
+            {
+              "hash": "string",
+              "block": 0,
+              "timestamp": "2018-11-13T20:48:28.782Z",
+              "confirmed": true,
+              "ownerAddress": "string",
+              "toAddress": "string",
+              "contractData": {
+                "boolean": true,
+                "number": true,
+                "string": true,
+                "object": true,
+                "array": true,
+                "null": true
+              },
+              "contractType": 0,
+              "data": "string"
+            }
     '''
     response = requests.get(scanendpoint + '/' + api + '/' +
                             "transaction", params = {"address" : address})
-    return response.json()["data"]
-
+    if (response):
+        return response.json()["data"]
+    return False
 
 def gettransfers(address):
     '''
+    GetTransfer API
 
-        GetTransfer API has following elements in each transaction :
+    :param address: The public address
+    :return:
+
+            A list of transfers (dictionaries) with the following info:
 
             {
               "id": "string",
@@ -63,40 +91,102 @@ def gettransfers(address):
               "tokenName": "string",
               "confirmed": true
             }
+    '''
+    response = requests.get(scanendpoint + '/' + api + '/' +
+                            "transfer", params = {"address" : address})
+    if (response):
+        return response.json()["data"]
+    return False
 
+
+def getlasttransfer(address):
+    '''
+
+    :param address:
+    :return:
+
+        A dictionary of info about the latest transfer
 
     '''
     response = requests.get(scanendpoint + '/' + api + '/' +
                             "transfer", params = {"address" : address})
-    return response.json()["data"]
+    if (response):
+        responsedata = response.json()["data"]
+        max = responsedata[0]["timestamp"];
+        lasttransfer = responsedata[0]
+        for transfer in responsedata:
+            if (transfer["timestamp"] > max):
+                max = transfer["timestamp"]
+                lasttransfer = transfer
+        return lasttransfer
+    return False
 
-
-def getlasttransfer(address):
-    response = requests.get(scanendpoint + '/' + api + '/' +
-                            "transfer", params = {"address" : address, "limit" : 1})
-    return response.json()["data"]
 
 def getlasttransactionobject(address):
-    return Transaction(getlasttransfer(address)[0])
+    '''
+
+    :param address:
+    :return:
+
+        A Transaction object with info about the latest transfer
+
+    '''
+    transferdata = getlasttransfer(address)
+    if (transferdata):
+        return Transaction(transferdata)
+    return False
 
 
 def gettransactionobjects(address):
+    '''
+
+    :param address:
+    :return:
+
+        A list of Transaction objects for all transactions
+
+    '''
     transactions = []
-    for transaction in gettransfers(address):
-        transactions.append(Transaction(transaction))
-    return transactions
+    rawtransfers = gettransfers(address)
+
+    if (rawtransfers):
+        for transaction in rawtransfers:
+            transactions.append(Transaction(transaction))
+        return transactions
+    return False
 
 
 def printtransfers(address):
+    '''
+
+    :param address:
+    :return:
+
+        Print all the current Transfer objects
+
+    '''
     for transfer in gettransfers(address):
         print(Transaction(transfer))
 
 
-
-
 def formatBalance(balance):
+    '''
+        Convert API balance term to a traditional version
+
+    :param balance:
+            An integer with all 6 digits after decimal
+
+    :return:
+
+            A formatted number that looks like : 45.34
+    '''
     return float(str(balance)[:-4])/100
 
 
 def base58tohexstring(address):
+    '''
+
+    :param address:
+    :return:
+    '''
     return decode(address).hex()[:-8]
