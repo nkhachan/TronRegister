f6e1ec1d01c2a2ccdecfbd0e098d1ac80c1178a2
nkhachan <noopur.khachane@gmail.com>
2018-11-14 13:18:25 -0800

    last tkinter

--- MainPage.py
+++ MainPage.py
@@ -1,11 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 from LoginPage import *
 from TransactionPage import *
 
 class MainPage(tk.Frame):
 
     def __init__(self, *args, **kwargs):
-
         # Call parent constructor
         tk.Frame.__init__(self, *args, **kwargs)
 
@@ -13,20 +13,18 @@ class MainPage(tk.Frame):
         lpage = LoginPage(self)
         tpage = TransactionPage(self)
 
-
+        # Put both of the pages into this Frame container
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
-
         lpage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         tpage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-        buttonframe = tk.Frame(self)
-        buttonframe.pack(side="top", fill="x", expand=False)
-
+        # Frame for the buttons on the side bar
+        buttonframe = tk.Frame(self, bg = "black", padx = 10, pady = 10)
+        buttonframe.pack(side="left", fill="x", expand=True)
         lbutton = tk.Button(buttonframe, text="Login", command=lpage.lift)
         tbutton = tk.Button(buttonframe, text="Transaction", command=tpage.lift)
-
-        lbutton.pack(side="left")
-        tbutton.pack(side="left")
+        lbutton.pack(side="left", padx = 10, pady = 10)
+        tbutton.pack(side="left", padx = 10, pady = 10)
 
         lpage.show()
--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import json
 def main():
     #quickDemo()
     root = tk.Tk()
+
+    # eliminate the titlebar
+    #root.overrideredirect(1)
+
     mainpage = MainPage(root)
     mainpage.pack(side="top", fill="both", expand=True)
+
     root.wm_geometry("1500x1000")
     root.mainloop()
 
