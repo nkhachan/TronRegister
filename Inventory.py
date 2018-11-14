fc23027e259028c99468211c0ea7b9cb10ecac3c
nkhachan <noopur.khachane@gmail.com>
2018-11-14 11:16:14 -0800

    add inspiration

--- Inventory.py
+++ Inventory.py
@@ -1,9 +1,10 @@
+import json
 
 class Inventory:
 
     def __init__(self):
         with open('localdata/inventory.json', 'r') as myfile:
-            self.data = json.loads(myfile.read())
+            self.items = json.loads(myfile.read())
 
 
 inventory = Inventory()
