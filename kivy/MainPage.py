6ff50a615900b2c0f358612b71cea9361ed2fdc5
nkhachan <noopur.khachane@gmail.com>
2018-11-19 11:30:10 -0800

    images on sidepanel

--- kivy/MainPage.py
+++ kivy/MainPage.py
@@ -1,20 +1,54 @@
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
 from TransactionPage import TransactionPage
 from WalletPage import WalletPage
 
 
+class WalletImage(Image):
+    def __init__(self, **kwargs):
+        super(WalletImage, self).__init__(**kwargs)
+        self.source = "wallet.png"
+
+class WalletButton(ButtonBehavior, Image):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.source = "img/wallet.png"
+
+    def on_press(self):
+        self.parent.switchToWallet()
+
+class TransButton(ButtonBehavior, Image):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.source = "img/trans.png"
+
+    def on_press(self):
+        self.parent.switchToTrans()
+
 class SidePanelButtons(GridLayout):
     def __init__(self, parent=None):
-        super().__init__(cols=1, rows=2, size_hint_y = 0.25)
-        self.add_widget(Button(text='Wallet', on_press=parent.switchToWallet))
-        self.add_widget(Button(text='Transaction', on_press=parent.switchToTrans))
+        super().__init__(cols=1, rows=3, size_hint_y = 0.25)
+        #walletbutton = Button(text='Wallet', on_press=parent.switchToWallet)
+        #transbutton = Button(text='Transaction', on_press=parent.switchToTrans)
+        #self.add_widget(walletbutton)
+        #self.add_widget(transbutton)
+
+        self.add_widget(WalletButton(self))
+        self.add_widget(TransButton(self))
+
+    def switchToWallet(self, callback=None):
+        self.parent.switchToWallet(callback)
+
+    def switchToTrans(self, callback=None):
+        self.parent.switchToTrans(callback)
 
 
 class SidePanel(GridLayout):
     def __init__(self, parent=None, **kwargs):
-        super().__init__(cols=1, rows=3, size_hint_x = 0.15, **kwargs)
+        super().__init__(cols=1, rows=3, size_hint_x = 0.10, **kwargs)
         self.add_widget(SidePanelButtons(self))
         self.add_widget(Label(text=""))
 
