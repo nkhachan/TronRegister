50d16fa33deb4fc01d21a0b0b1bead8e3c18764f
nkhachan <noopur.khachane@gmail.com>
2018-12-07 13:10:49 -0800

    new library

--- src/PrintQRCode.py
+++ src/PrintQRCode.py
@@ -1,9 +1,12 @@
 from Adafruit_Thermal import *
+import sys
 import serial
 
 printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=3000)
 
+bill = sys.argv[1]
+
 printer.doubleHeightOn()
-printer.println("Double Height ON")
+printer.println(bill.toString())
 from qr import *
 printer.printBitmap(width, height, data)
--- src/Printer.py
+++ src/Printer.py
@@ -1,47 +1,13 @@
 from Bill import bill
 from CoinMarketCap import *
 
-import adafruit_thermal_printer
 import serial
+import subprocess
+
 
 def printOutFinalBill():
 
-    ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.67)
-    uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
-    printer = ThermalPrinter(uart)
-    printer.warm_up()
-
-    printer.size = adafruit_thermal_printer.SIZE_LARGE
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.underline = adafruit_thermal_printer.UNDERLINE_THICK
-
-    printer.print('Garam Market!')
-
-    printer.underline = None
-    printer.size = adafruit_thermal_printer.SIZE_SMALL
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.underline = adafruit_thermal_printer.UNDERLINE_THICK
-    printer.print('9700 Gilman Dr')
-    printer.print('La Jolla, CA, 92093')
-
-    printer.feed(2)
-
-    printer.print(' Product       Quantity    Price')
-    printer.feed(1)
-    # printer.underline = adafruit_thermal_printer.UNDERLINE_THICK
-    printer.print('-------------------------------')
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.print(bill.toString())
-    printer.print('-------------------------------')
-
-    printer.bold = True
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.print('Total: ')
-    printer.bold = False
-
-    printer.justify = adafruit_thermal_printer.JUSTIFY_RIGHT
-    printer.print(str(round(bill.trxTotal(), 2)))
-
-    printer.feed(2)
-
-# QR Code
+    script = ["python2.7", "PrintQRCode.py", bill]
+    process = subprocess.Popen(" ".join(script),
+                               shell=True,
+                               env={"PYTHONPATH": "."})
