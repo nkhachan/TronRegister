721bf3bbfd2060dd78b7d97a657f56eb06cd1f5f
nkhachan <noopur.khachane@gmail.com>
2018-12-07 13:03:42 -0800

    remove stuff

--- src/PrintQRCode.py
+++ src/PrintQRCode.py
@@ -1,17 +1,6 @@
 from Adafruit_Thermal import *
 import serial
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data("TMZtJCtmk7ykoCFn5WTSKbedgv8xFuucfz")
-qr.make(fit=True)
-
-img = qr.make_image(fill_color="black", back_color="white")
-
 printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=3000)
 
 printer.doubleHeightOn()
