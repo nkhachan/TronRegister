347a3ef4c71e7fedc9f7cec956c5a2dd8a427e05
nkhachan <noopur.khachane@gmail.com>
2018-12-07 12:03:27 -0800

    meh

--- src/PrintQRCode.py
+++ src/PrintQRCode.py
@@ -1,8 +1,10 @@
 from Adafruit_Thermal import *
 import serial
 
-def printOutFinalBill():
-    printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=3000)
+printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=3000)
 
-    printer.doubleHeightOn()
-    printer.println("Double Height ON")
+printer.doubleHeightOn()
+printer.println("Double Height ON")
+from logo import *
+
+printer.printBitmap(width, height, data)
